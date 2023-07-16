51cb11a5eedffc436387a22485e0aa772e2e19a7
KIL4ME <pdo323@yahoo.com>
2023-07-16 10:25:33 -0700

    deleting error

--- JS/script.js
+++ JS/script.js
@@ -2,14 +2,11 @@ function Pizza(toppings, size) {
     this.toppings = toppings;
     this.size = size;
 }
-
 Pizza.prototype.calculateCost = function() {
-    var baseCost = 5; // Base cost of the pizza
+    var baseCost = 5;
   
-    // Add cost based on the number of toppings
     var toppingCost = this.toppings.length * 1.5;
   
-    // Add cost based on the size of the pizza
     var sizeCost;
     switch (this.size) {
       case "small":
